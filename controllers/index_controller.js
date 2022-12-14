0d05119b526d5e42dfb5c9bee7a9fda1c7046bb8
harsh6975 <harshu1234ashv@gmail.com>
2022-12-15 01:17:17 +0530

    fetching price at every 10min

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const Price = require("../models/priceSchema");
 
 module.exports.price = async function (req, res) {
   //async await
@@ -7,7 +8,13 @@ module.exports.price = async function (req, res) {
       const getPrice = await axios.get(
         "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=inr"
       );
-    }, 1000*60*10); 
+      const value = getPrice.data.ethereum.inr;
+      let price = await Price.create({
+        id: "ethereum",
+        currency: "inr",
+        value: value,
+      });
+    }, 1000 * 60 * 10);
 
     return res.send({
       success: true,
